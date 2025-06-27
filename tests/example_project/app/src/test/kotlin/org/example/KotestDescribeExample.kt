6cb83be3338af78b505406069fe7ef12180214cc
Nick Hackman <snickhackman@gmail.com>
2025-06-27 17:36:43 -0400

    refactor: move tests to top level
    This follows other neotest plugin project structures.

---
+++ tests/example_project/app/src/test/kotlin/org/example/KotestDescribeExample.kt
@@ -0,0 +1,41 @@
+
+package org.example
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+
+class KotestDescribeSpec : DescribeSpec({
+
+    describe("a namespace") {
+
+       it("should handle failed assertions") {
+         "a" shouldBe "b"
+       }
+
+       it("should handle passed assertions") {
+         "a" shouldBe "a"
+       }
+
+       xit("should handle skipped assertions") {
+        "a" shouldBe "a"
+       }
+
+      describe("a nested namespace") {
+
+        it("should handle failed assertions") {
+          "a" shouldBe "b"
+        }
+
+        it("should handle passed assertions") {
+          "a" shouldBe "a"
+        }
+
+        xit("should handle skipped assertions") {
+          "a" shouldBe "a"
+        }
+
+       }
+
+    }
+})
