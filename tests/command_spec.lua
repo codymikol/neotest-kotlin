fe2c1fc085e2b456a1abaecb1a21a04750d33cfa
Nick Hackman <snickhackman@gmail.com>
2025-06-27 17:36:43 -0400

    test: remove doubly nested namespaces

--- tests/command_spec.lua
+++ tests/command_spec.lua
@@ -1,23 +1,18 @@
 local command = require("neotest-kotlin.command")
 
 describe("command", function()
-	describe("building a gradle command", function()
-		it("valid", function()
-			local actual = command.build(
-				"An example namespace",
-				"com.codymikol.gummibear.pizza.FooClass",
-				"/tmp/results_example.txt"
-			)
+	it("valid", function()
+		local actual =
+			command.build("An example namespace", "com.codymikol.gummibear.pizza.FooClass", "/tmp/results_example.txt")
 
-			local init_script_path = vim.api.nvim_get_runtime_file("test-logging.init.gradle.kts", false)[1]
+		local init_script_path = vim.api.nvim_get_runtime_file("test-logging.init.gradle.kts", false)[1]
 
-			assert.equals(
-				string.format(
-					"kotest_filter_specs='com.codymikol.gummibear.pizza.FooClass' kotest_filter_tests='An example namespace' ./gradlew -I %s test --console=plain | tee -a /tmp/results_example.txt",
-					init_script_path
-				),
-				actual
-			)
-		end)
+		assert.equals(
+			string.format(
+				"kotest_filter_specs='com.codymikol.gummibear.pizza.FooClass' kotest_filter_tests='An example namespace' ./gradlew -I %s test --console=plain | tee -a /tmp/results_example.txt",
+				init_script_path
+			),
+			actual
+		)
 	end)
 end)
