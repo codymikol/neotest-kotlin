7f7c3ca8d0bca1f43c19cf6f2ee690b39a34075b
Nick Hackman <snickhackman@gmail.com>
2025-07-08 00:55:41 -0400

    feat: remove appending " should" and " when"

--- lua/neotest-kotlin/treesitter/init.lua
+++ lua/neotest-kotlin/treesitter/init.lua
@@ -6,6 +6,24 @@ local package_query = require("neotest-kotlin.treesitter.package-query")
 
 local M = {}
 
+---@enum neotest.PositionType
+M.PositionType = {
+  dir = "dir",
+  file = "file",
+  namespace = "namespace",
+  test = "test",
+}
+
+---@class neotest.Position
+---@field id string
+---@field type neotest.PositionType
+---@field name string
+---@field path string
+---@field range integer[]
+
+---@class Position : neotest.Position
+---@field custom_id string a custom id that differs from the name
+
 ---Get all matches for the query perform on the path
 ---@param path string
 ---@param query string
@@ -62,6 +80,62 @@ function M.java_package(file)
   return get_all_matches_as_string(file, package_query)[1]
 end
 
+---Creates a neotest id of the form 'path::namespace::test' using the Position.custom_id if it exists otherwise name.
+---@param position Position
+---@param parents Position[]
+---@return string
+local function position_id(position, parents)
+  return table.concat(
+    vim
+      .iter({
+        position.path,
+        ---@param pos Position
+        vim.tbl_map(function(pos)
+          return pos.custom_id or pos.name
+        end, parents),
+        position.custom_id or position.name,
+      })
+      :flatten(math.huge)
+      :totable(),
+    "::"
+  )
+end
+
+---builds a neotest.Position from a treesitter query
+---@param file_path string
+---@param source string
+---@param captured_nodes table<string, userdata>
+---@param metadata table<string, vim.treesitter.query.TSMetadata>
+---@return Position
+local function build_position(file_path, source, captured_nodes, metadata)
+  ---@param captured_nodes table<string, userdata>
+  local function get_match_type(captured_nodes)
+    if captured_nodes["test.name"] then
+      return "test"
+    end
+    if captured_nodes["namespace.name"] then
+      return "namespace"
+    end
+  end
+
+  local match_type = get_match_type(captured_nodes)
+  if match_type then
+    local node_name = match_type .. ".name"
+    ---@type string
+    local name = vim.treesitter.get_node_text(captured_nodes[node_name], source)
+
+    local definition = captured_nodes[match_type .. ".definition"]
+
+    return {
+      type = match_type,
+      path = file_path,
+      name = name,
+      custom_id = metadata[node_name] and metadata[node_name].text,
+      range = { definition:range() },
+    }
+  end
+end
+
 ---Uses neotest.treeistter.parse_positions to discover all namespaces/tests in a file.
 ---@param file string
 ---@return neotest.Tree?
@@ -69,39 +143,8 @@ function M.parse_positions(file)
   return neotest.treesitter.parse_positions(file, kotest_query, {
     nested_namespaces = true,
     nested_tests = false,
-    ---@type fun(file_path: string, source: string, captured_nodes: table<string, userdata>, metadata: table<string, vim.treesitter.query.TSMetadata>): neotest.Position|neotest.Position[]|nil Builds one or more positions from the captured nodes from a query match.
-    build_position = function(file_path, source, captured_nodes, metadata)
-      ---@param captured_nodes table<string, userdata>
-      local function get_match_type(captured_nodes)
-        if captured_nodes["test.name"] then
-          return "test"
-        end
-        if captured_nodes["namespace.name"] then
-          return "namespace"
-        end
-      end
-
-      local match_type = get_match_type(captured_nodes)
-      if match_type then
-        local node_name = match_type .. ".name"
-        ---@type string
-        local name =
-          vim.treesitter.get_node_text(captured_nodes[node_name], source)
-
-        if metadata[node_name] ~= nil and metadata[node_name].text ~= nil then
-          name = metadata[node_name].text
-        end
-
-        local definition = captured_nodes[match_type .. ".definition"]
-
-        return {
-          type = match_type,
-          path = file_path,
-          name = name,
-          range = { definition:range() },
-        }
-      end
-    end,
+    build_position = build_position,
+    position_id = position_id,
   })
 end
 
