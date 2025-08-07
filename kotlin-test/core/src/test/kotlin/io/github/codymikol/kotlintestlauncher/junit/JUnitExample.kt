7ebdb1f068abec5cf67567e99d5713cf0c681ee2
Nick Hackman <snickhackman@gmail.com>
2025-08-07 19:39:12 -0400

    test: JUnit5 test running

---
+++ kotlin-test/core/src/test/kotlin/io/github/codymikol/kotlintestlauncher/junit/JUnitExample.kt
@@ -0,0 +1,77 @@
+package io.github.codymikol.kotlintestlauncher.junit
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.assertAll
+
+class JUnitExample {
+    @Test
+    fun pass() {
+        assertEquals(1, 1)
+    }
+
+    @Test
+    fun fail() {
+        assertEquals(1, 2)
+    }
+
+    @Nested
+    inner class NestedJUnitExample {
+        @Test
+        fun pass() {
+            assertEquals(1, 1)
+        }
+
+        @Test
+        fun fail() {
+            assertEquals(1, 2)
+        }
+    }
+
+    @Test
+    @DisplayName("test name with spaces")
+    fun nameIsReplacedWithDisplayName() {
+        assertEquals(1, 1)
+    }
+
+    @Test
+    @Disabled
+    fun disabled() {
+        assertEquals(1, 3)
+    }
+
+    @Test
+    fun softAssertions() {
+        assertAll(
+            { assertEquals(1, 4) },
+            { assertEquals(1, 4) },
+            { assertEquals(1, 4) },
+            { assertEquals(1, 4) },
+            { assertEquals(1, 4) },
+        )
+    }
+
+    @Test
+    fun `kotlin escaped string pass`() {
+        assertEquals(1, 1)
+    }
+
+    @TestFactory
+    fun testSquares() =
+        listOf(
+            1 to 1,
+            2 to 4,
+            3 to 9,
+            4 to 16,
+            5 to 25,
+        ).map { (input, expected) ->
+            DynamicTest.dynamicTest("$input^2 = $expected") {
+                assertEquals(expected, input * input)
+            }
+        }
+}
---
+++ kotlin-test/core/src/test/kotlin/io/github/codymikol/kotlintestlauncher/junit/JUnitTestRunnerFunctionalSpec.kt
@@ -0,0 +1,158 @@
+package io.github.codymikol.kotlintestlauncher.junit
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.github.codymikol.kotlintestlauncher.TestRunResult
+import io.kotest.assertions.json.shouldContainJsonKey
+import io.kotest.assertions.json.shouldEqualSpecifiedJson
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.types.shouldBeInstanceOf
+
+class JUnitTestRunnerFunctionalSpec :
+    FunSpec({
+        context("functional") {
+            test("run") {
+                val result = JUnitTestRunner.run(listOf(JUnitExample::class))
+                val actual = result.shouldBeInstanceOf<TestRunResult.Success>()
+                val actualJson = ObjectMapper().registerKotlinModule().writeValueAsString(actual.report)
+
+                actualJson.shouldContainJsonKey("$[0].tests[0].duration")
+                actualJson.shouldContainJsonKey("$[0].tests[0].status.stackTrace")
+
+                actualJson shouldEqualSpecifiedJson
+                    """
+                    [
+                      {
+                        "name": "io.github.codymikol.kotlintestlauncher.junit.JUnitExample",
+                        "type": "CONTAINER",
+                        "status": "FAILURE",
+                        "tests": [
+                          {
+                            "name": "fail()",
+                            "status": {
+                              "error": {
+                                "message": "expected: <1> but was: <2>",
+                                "lineNumber": 151,
+                                "filename": "AssertionFailureBuilder.java"
+                              },
+                              "type": "FAILURE"
+                            },
+                            "type": "TEST"
+                          },
+                          {
+                            "name": "pass()",
+                            "status": {
+                              "type": "SUCCESS"
+                            },
+                            "type": "TEST"
+                          },
+                          {
+                            "name": "disabled()",
+                            "duration": 0,
+                            "status": {
+                              "reason": "public final void io.github.codymikol.kotlintestlauncher.junit.JUnitExample.disabled() is @Disabled",
+                              "type": "IGNORED"
+                            },
+                            "type": "TEST"
+                          },
+                          {
+                            "name": "test name with spaces",
+                            "status": {
+                              "type": "SUCCESS"
+                            },
+                            "type": "TEST"
+                          },
+                          {
+                            "name": "softAssertions()",
+                            "status": {
+                              "error": {
+                                "message": "Multiple Failures (5 failures)\n\torg.opentest4j.AssertionFailedError: expected: <1> but was: <4>\n\torg.opentest4j.AssertionFailedError: expected: <1> but was: <4>\n\torg.opentest4j.AssertionFailedError: expected: <1> but was: <4>\n\torg.opentest4j.AssertionFailedError: expected: <1> but was: <4>\n\torg.opentest4j.AssertionFailedError: expected: <1> but was: <4>",
+                                "lineNumber": 80,
+                                "filename": "AssertAll.java"
+                              },
+                              "type": "FAILURE"
+                            },
+                            "type": "TEST"
+                          },
+                          {
+                            "name": "kotlin escaped string pass()",
+                            "status": {
+                              "type": "SUCCESS"
+                            },
+                            "type": "TEST"
+                          },
+                          {
+                            "name": "testSquares()",
+                            "type": "CONTAINER",
+                            "status": "SUCCESS",
+                            "tests": [
+                              {
+                                "name": "1^2 = 1",
+                                "status": {
+                                  "type": "SUCCESS"
+                                },
+                                "type": "TEST"
+                              },
+                              {
+                                "name": "2^2 = 4",
+                                "status": {
+                                  "type": "SUCCESS"
+                                },
+                                "type": "TEST"
+                              },
+                              {
+                                "name": "3^2 = 9",
+                                "status": {
+                                  "type": "SUCCESS"
+                                },
+                                "type": "TEST"
+                              },
+                              {
+                                "name": "4^2 = 16",
+                                "status": {
+                                  "type": "SUCCESS"
+                                },
+                                "type": "TEST"
+                              },
+                              {
+                                "name": "5^2 = 25",
+                                "status": {
+                                  "type": "SUCCESS"
+                                },
+                                "type": "TEST"
+                              }
+                            ]
+                          },
+                          {
+                            "name": "io.github.codymikol.kotlintestlauncher.junit.JUnitExample${'$'}NestedJUnitExample",
+                            "type": "CONTAINER",
+                            "status": "FAILURE",
+                            "tests": [
+                              {
+                                "name": "fail()",
+                                "status": {
+                                  "error": {
+                                    "message": "expected: <1> but was: <2>",
+                                    "lineNumber": 151,
+                                    "filename": "AssertionFailureBuilder.java"
+                                  },
+                                  "type": "FAILURE"
+                                },
+                                "type": "TEST"
+                              },
+                              {
+                                "name": "pass()",
+                                "status": {
+                                  "type": "SUCCESS"
+                                },
+                                "type": "TEST"
+                              }
+                            ]
+                          }
+                        ]
+                      }
+                    ]
+                    """.trimIndent()
+            }
+        }
+    })
