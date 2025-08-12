98bdc2ebaf78e0ae9e329f52a5b13a9042a429bf
Nick Hackman <snickhackman@gmail.com>
2025-08-12 17:20:27 -0400

    test(command): update with filter and without filter tests

--- tests/command_spec.lua
+++ tests/command_spec.lua
@@ -1,9 +1,9 @@
 local command = require("neotest-kotlin.command")
 
 describe("command", function()
-  it("valid", function()
+  it("no filter", function()
     local actual =
-      command.build("An example namespace", "/tmp/results_example.json")
+      command.build("An example namespace", nil, "/tmp/results_example.json")
 
     local init_script_path =
       vim.api.nvim_get_runtime_file("test-logging.init.gradle.kts", false)[1]
@@ -11,6 +11,25 @@ describe("command", function()
     assert.equals(
       string.format(
         "./gradlew -I %s kotlinTest -Pclasses=An example namespace -PoutputFile=/tmp/results_example.json",
+        init_script_path
+      ),
+      actual
+    )
+  end)
+
+  it("filter", function()
+    local actual = command.build(
+      "An example namespace",
+      "org.example.TestExample::pass",
+      "/tmp/results_example.json"
+    )
+
+    local init_script_path =
+      vim.api.nvim_get_runtime_file("test-logging.init.gradle.kts", false)[1]
+
+    assert.equals(
+      string.format(
+        "./gradlew -I %s kotlinTest -Pclasses=An example namespace -PoutputFile=/tmp/results_example.json -Pfilter=org.example.TestExample::pass",
         init_script_path
       ),
       actual
