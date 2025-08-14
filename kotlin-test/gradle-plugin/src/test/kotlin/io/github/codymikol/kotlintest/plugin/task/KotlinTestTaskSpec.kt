7bb327288f44821a53b1b7310bdc79bcb03eb171
Nick Hackman <snickhackman@gmail.com>
2025-08-13 20:35:14 -0400

    test: Path.isTopLevelClass

---
+++ kotlin-test/gradle-plugin/src/test/kotlin/io/github/codymikol/kotlintest/plugin/task/KotlinTestTaskSpec.kt
@@ -0,0 +1,20 @@
+package io.github.codymikol.kotlintest.plugin.task
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+import kotlin.io.path.Path
+
+class KotlinTestTaskSpec : FunSpec ({
+    context("Path.isTopLevelClass") {
+        withData(
+            mapOf(
+                "Ends with .class" to Pair(Path("Example.class"), true),
+                "Nested class" to Pair(Path($$"Example$Inner.class"), false),
+                "Not Java class" to Pair(Path("RandomFile.json"), false),
+            )
+        ) { (input, expected) ->
+            input.isTopLevelClass() shouldBe expected
+        }
+    }
+})
