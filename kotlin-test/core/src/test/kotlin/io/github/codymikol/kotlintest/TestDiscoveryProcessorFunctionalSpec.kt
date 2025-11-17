7565a2d41c672b34bb268b4f765e45222af9c2f9
Nick Hackman <snickhackman@gmail.com>
2025-11-16 19:43:14 -0500

    test: base cases for Kotest FunSpec

--- kotlin-test/core/src/test/kotlin/io/github/codymikol/kotlintest/TestDiscoveryProcessorFunctionalSpec.kt
+++ kotlin-test/core/src/test/kotlin/io/github/codymikol/kotlintest/TestDiscoveryProcessorFunctionalSpec.kt
@@ -1,28 +1,15 @@
 package io.github.codymikol.kotlintest
 
-import com.intellij.lang.Language
-import com.intellij.mock.MockProject
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiFileFactory
-import com.intellij.testFramework.LightVirtualFile
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import org.jetbrains.kotlin.analysis.api.projectStructure.KaSourceModule
 import org.jetbrains.kotlin.analysis.api.standalone.buildStandaloneAnalysisAPISession
-import org.jetbrains.kotlin.analysis.project.structure.builder.KtSourceModuleBuilder
-import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtLibraryModule
 import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtSourceModule
-import org.jetbrains.kotlin.cli.common.CliModuleVisibilityManagerImpl
-import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreApplicationEnvironment
-import org.jetbrains.kotlin.fir.declarations.builder.buildFile
-import org.jetbrains.kotlin.idea.KotlinLanguage
-import org.jetbrains.kotlin.konan.file.createTempFile
-import org.jetbrains.kotlin.load.kotlin.ModuleVisibilityManager
 import org.jetbrains.kotlin.platform.jvm.JvmPlatforms
 import org.jetbrains.kotlin.psi.KtFile
+import com.intellij.testFramework.LightVirtualFile
+import org.jetbrains.kotlin.idea.KotlinLanguage
 
 internal fun createKtFile(filename: String, code: String): KtFile {
     val session = buildStandaloneAnalysisAPISession(unitTestMode = true) {
@@ -45,6 +32,16 @@ internal fun createKtFile(filename: String, code: String): KtFile {
 }
 
 class TestDiscoveryProcessorFunctionalSpec : FunSpec ({
+    lateinit var disposable: Disposable
+
+    beforeTest {
+        disposable = Disposer.newDisposable()
+    }
+
+    afterTest {
+        disposable.dispose()
+    }
+
     test("top-level test") {
         val ktFile = createKtFile("ExampleFunSpec.kt", """
             import io.kotest.core.spec.style.FunSpec
@@ -110,6 +107,66 @@ class TestDiscoveryProcessorFunctionalSpec : FunSpec ({
         )
     }
 
+    test("deeply nested test") {
+        val ktFile = createKtFile("ExampleFunSpec.kt", """
+            import io.kotest.core.spec.style.FunSpec
+            import io.kotest.matchers.shouldBe
+            
+            class ExampleFunSpec : FunSpec({
+                context("container1") {
+                    context("container2") {
+                        context("container3") {
+                            test("test") {
+                              1 shouldBe 1
+                            }
+                        }
+                    }
+                }
+            })
+        """.trimIndent())
+
+        val results = KotestTestDiscoverer.discoverTests(ktFile)
+
+        results shouldBe setOf(
+            DiscoveredTest(
+                id = "container1",
+                type = TestType.CONTAINER,
+                position = Position(
+                    filename = "/ExampleFunSpec.kt",
+                    start = 5,
+                    end = 13
+                )
+            ),
+            DiscoveredTest(
+                id = "container1::container2",
+                type = TestType.CONTAINER,
+                position = Position(
+                    filename = "/ExampleFunSpec.kt",
+                    start = 6,
+                    end = 12
+                )
+            ),
+            DiscoveredTest(
+                id = "container1::container2::container3",
+                type = TestType.CONTAINER,
+                position = Position(
+                    filename = "/ExampleFunSpec.kt",
+                    start = 7,
+                    end = 11
+                )
+            ),
+            DiscoveredTest(
+                id = "container1::container2::container3::test",
+                type = TestType.TEST,
+                position = Position(
+                    filename = "/ExampleFunSpec.kt",
+                    start = 8,
+                    end = 10
+                )
+            )
+        )
+    }
+
     test("multiple top-level test") {
         val ktFile = createKtFile("ExampleFunSpec.kt", """
             import io.kotest.core.spec.style.FunSpec
@@ -160,6 +217,105 @@ class TestDiscoveryProcessorFunctionalSpec : FunSpec ({
                     end = 15
                 )
             )
+        )
+    }
+
+    test("complex test suite") {
+        val ktFile = createKtFile("ExampleFunSpec.kt", """
+            import io.kotest.core.spec.style.FunSpec
+            import io.kotest.matchers.shouldBe
+            
+            class ExampleFunSpec : FunSpec({
+                context("container1") {
+                    test("test1") {
+                        1 shouldBe 1
+                    }
+                }
+                
+                context("container2") {
+                    context("container3") {
+                        test("test2") {
+                            1 shouldBe 1
+                        }
+                    }
+                    
+                    test("test3") {
+                        1 shouldBe 1
+                    }
+                }
+                
+                test("test4") {
+                  1 shouldBe 1
+                }
+            })
+        """.trimIndent())
+
+        val results = KotestTestDiscoverer.discoverTests(ktFile)
+
+        results shouldBe setOf(
+            DiscoveredTest(
+                id = "container1",
+                type = TestType.CONTAINER,
+                position = Position(
+                    filename = "/ExampleFunSpec.kt",
+                    start = 5,
+                    end = 9
+                )
+            ),
+            DiscoveredTest(
+                id = "container1::test1",
+                type = TestType.TEST,
+                position = Position(
+                    filename = "/ExampleFunSpec.kt",
+                    start = 6,
+                    end = 8
+                )
+            ),
+            DiscoveredTest(
+                id = "container2",
+                type = TestType.CONTAINER,
+                position = Position(
+                    filename = "/ExampleFunSpec.kt",
+                    start = 11,
+                    end = 21
+                )
+            ),
+            DiscoveredTest(
+                id = "container2::container3",
+                type = TestType.CONTAINER,
+                position = Position(
+                    filename = "/ExampleFunSpec.kt",
+                    start = 12,
+                    end = 16
+                )
+            ),
+            DiscoveredTest(
+                id = "container2::container3::test2",
+                type = TestType.TEST,
+                position = Position(
+                    filename = "/ExampleFunSpec.kt",
+                    start = 13,
+                    end = 15
+                )
+            ),
+            DiscoveredTest(
+                id = "container2::test3",
+                type = TestType.TEST,
+                position = Position(
+                    filename = "/ExampleFunSpec.kt",
+                    start = 18,
+                    end = 20
+                )
+            ),
+            DiscoveredTest(
+                id = "test4",
+                type = TestType.TEST,
+                position = Position(
+                    filename = "/ExampleFunSpec.kt",
+                    start = 23,
+                    end = 25
+                )
+            ),
         )
     }
 })
