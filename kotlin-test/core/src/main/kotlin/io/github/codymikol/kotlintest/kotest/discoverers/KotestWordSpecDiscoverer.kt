c4518c6c7e39a4b9e759db6a3ba17050e5041f82
Nick Hackman <snickhackman@gmail.com>
2025-11-18 23:01:58 -0500

    fix(WordSpec): assert that "should" is the operator for containers

--- kotlin-test/core/src/main/kotlin/io/github/codymikol/kotlintest/kotest/discoverers/KotestWordSpecDiscoverer.kt
+++ kotlin-test/core/src/main/kotlin/io/github/codymikol/kotlintest/kotest/discoverers/KotestWordSpecDiscoverer.kt
@@ -25,6 +25,10 @@ internal object KotestWordSpecDiscoverer : KotestTestTypeDiscoverer {
             ?.flatMap { expression ->
                 when (expression) {
                     is KtBinaryExpression -> {
+                        if (expression.operationReference.text != "should") {
+                            return@flatMap emptyList()
+                        }
+
                         val container = DiscoveredTest(
                             id = expression.firstChild.text.trim('"'),
                             position = expression.determinePosition(),
