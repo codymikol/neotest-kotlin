97fd40fb707bd7f4bbee4d1884e0e6521f5483a4
Nick Hackman <snickhackman@gmail.com>
2026-01-01 14:20:30 -0500

    style: remove unnecessary listOfNotNull

--- kotlin-test/gradle-plugin/src/main/kotlin/io/github/codymikol/kotlintest/plugin/task/KotlinTestDiscoverTask.kt
+++ kotlin-test/gradle-plugin/src/main/kotlin/io/github/codymikol/kotlintest/plugin/task/KotlinTestDiscoverTask.kt
@@ -24,7 +24,7 @@ abstract class KotlinTestDiscoverTask : JavaExec() {
         println("Executing: $MAIN discover --files=$files --output=$outputFile")
 
         this.args(
-            listOfNotNull(
+            listOf(
                 "discover",
                 "--files=$files",
                 "--output=$outputFile",
