a88507f91133574baa126916c98ec5fd50d7d8c2
Nick Hackman <snickhackman@gmail.com>
2026-01-04 02:47:12 -0500

    test: update with different command building

--- tests/command_spec.lua
+++ tests/command_spec.lua
@@ -1,38 +1,71 @@
 local command = require("neotest-kotlin.command")
 
 describe("command", function()
-  it("no filter", function()
-    local actual =
-      command.build("An example namespace", nil, "/tmp/results_example.json")
+  local init_script_path = vim.fn.fnamemodify(
+    vim.api.nvim_get_runtime_file("test-logging.init.gradle.kts", false)[1],
+    ":p"
+  )
 
-    local init_script_path =
-      vim.api.nvim_get_runtime_file("test-logging.init.gradle.kts", false)[1]
+  describe("build_discover", function()
+    it("no file", function()
+      local actual_command, actual_args =
+        command.build_discover(nil, "/tmp/discover_results_example.json")
 
-    assert.equals(
-      string.format(
-        "./gradlew -I %s kotlinTest -Pclasses='An example namespace' -PoutputFile='/tmp/results_example.json'",
-        init_script_path
-      ),
-      actual
-    )
+      assert.equals("./gradlew", actual_command)
+      assert.are.same({
+        "-I",
+        init_script_path,
+        "kotlinTestDiscover",
+        "-PoutputFile=/tmp/discover_results_example.json",
+      }, actual_args)
+    end)
+
+    it("file", function()
+      local actual_command, actual_args =
+        command.build_discover("file", "/tmp/discover_results_example.json")
+
+      assert.equals("./gradlew", actual_command)
+      assert.are.same({
+        "-I",
+        init_script_path,
+        "kotlinTestDiscover",
+        "-PoutputFile=/tmp/discover_results_example.json",
+        "-Pfile=file",
+      }, actual_args)
+    end)
   end)
 
-  it("filter", function()
-    local actual = command.build(
-      "An example namespace",
-      "org.example.TestExample::pass",
-      "/tmp/results_example.json"
-    )
+  describe("build_execute", function()
+    it("no filter", function()
+      local actual = command.build_execute(
+        "An example namespace",
+        nil,
+        "/tmp/results_example.json"
+      )
 
-    local init_script_path =
-      vim.api.nvim_get_runtime_file("test-logging.init.gradle.kts", false)[1]
+      assert.equals(
+        string.format(
+          "./gradlew -I %s kotlinTestExecute -Pclasses='An example namespace' -PoutputFile='/tmp/results_example.json'",
+          init_script_path
+        ),
+        actual
+      )
+    end)
 
-    assert.equals(
-      string.format(
-        "./gradlew -I %s kotlinTest -Pclasses='An example namespace' -PoutputFile='/tmp/results_example.json' -Pfilter='org.example.TestExample::pass'",
-        init_script_path
-      ),
-      actual
-    )
+    it("filter", function()
+      local actual = command.build_execute(
+        "An example namespace",
+        "org.example.TestExample::pass",
+        "/tmp/results_example.json"
+      )
+
+      assert.equals(
+        string.format(
+          "./gradlew -I %s kotlinTestExecute -Pclasses='An example namespace' -PoutputFile='/tmp/results_example.json' -Pfilter='org.example.TestExample::pass'",
+          init_script_path
+        ),
+        actual
+      )
+    end)
   end)
 end)
