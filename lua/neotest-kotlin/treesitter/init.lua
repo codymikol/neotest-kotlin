f16e6b38a518334e817316d2d3f3e967f6c048ea
Nick Hackman <snickhackman@gmail.com>
2026-01-04 02:46:26 -0500

    style: remove unused code
    No longer used as the gradle plugin now performs discovery.

--- lua/neotest-kotlin/treesitter/init.lua
+++ lua/neotest-kotlin/treesitter/init.lua
@@ -1,7 +1,4 @@
-local neotest = require("neotest.lib")
-
 local class_query = require("neotest-kotlin.treesitter.class-query")
-local kotest_query = require("neotest-kotlin.treesitter.kotest-query")
 local package_query = require("neotest-kotlin.treesitter.package-query")
 
 local M = {}
@@ -78,74 +75,6 @@ end
 ---@return string? package
 function M.java_package(file)
   return get_all_matches_as_string(file, package_query)[1]
-end
-
----Creates a neotest id of the form 'path::namespace::test' using the Position.custom_id if it exists otherwise name.
----@param position Position
----@param parents Position[]
----@return string
-local function position_id(position, parents)
-  return table.concat(
-    vim
-      .iter({
-        position.path,
-        ---@param pos Position
-        vim.tbl_map(function(pos)
-          return pos.custom_id or pos.name
-        end, parents),
-        position.custom_id or position.name,
-      })
-      :flatten(math.huge)
-      :totable(),
-    "::"
-  )
-end
-
----builds a neotest.Position from a treesitter query
----@param file_path string
----@param source string
----@param captured_nodes table<string, userdata>
----@param metadata table<string, vim.treesitter.query.TSMetadata>
----@return Position
-local function build_position(file_path, source, captured_nodes, metadata)
-  ---@param captured_nodes table<string, userdata>
-  local function get_match_type(captured_nodes)
-    if captured_nodes["test.name"] then
-      return "test"
-    end
-    if captured_nodes["namespace.name"] then
-      return "namespace"
-    end
-  end
-
-  local match_type = get_match_type(captured_nodes)
-  if match_type then
-    local node_name = match_type .. ".name"
-    ---@type string
-    local name = vim.treesitter.get_node_text(captured_nodes[node_name], source)
-
-    local definition = captured_nodes[match_type .. ".definition"]
-
-    return {
-      type = match_type,
-      path = file_path,
-      name = name,
-      custom_id = metadata[node_name] and metadata[node_name].text,
-      range = { definition:range() },
-    }
-  end
-end
-
----Uses neotest.treeistter.parse_positions to discover all namespaces/tests in a file.
----@param file string
----@return neotest.Tree?
-function M.parse_positions(file)
-  return neotest.treesitter.parse_positions(file, kotest_query, {
-    nested_namespaces = true,
-    nested_tests = false,
-    build_position = build_position,
-    position_id = position_id,
-  })
 end
 
 return M
