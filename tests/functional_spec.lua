4545d4e548dce0af0306ddbc63a158347441f79d
Nick Hackman <snickhackman@gmail.com>
2026-01-04 02:47:26 -0500

    test(Discovery): DescribeSpec functional test

---
+++ tests/functional_spec.lua
@@ -0,0 +1,91 @@
+local neotest_kotlin = require("neotest-kotlin")
+local nio = require("nio")
+
+describe("discover_positions", function()
+  local example_project_path = vim.fs.joinpath(
+    debug.getinfo(1).source:match("@?(.*/)"),
+    "example_project",
+    "app",
+    "src",
+    "test",
+    "kotlin",
+    "org",
+    "example"
+  )
+
+  nio.tests.it("example", function()
+    local test_path =
+      vim.fs.joinpath(example_project_path, "KotestDescribeSpec.kt")
+
+    ---@type any[]
+    local tree = neotest_kotlin.discover_positions(test_path):to_list()
+
+    assert.are.same({
+      id = test_path,
+      path = test_path,
+      name = "KotestDescribeSpec.kt",
+      range = {
+        0,
+        0,
+        34,
+        0,
+      },
+      type = "file",
+    }, tree[1])
+
+    assert.are.same({
+      id = test_path
+        .. "::"
+        .. "org.example.KotestDescribeSpec"
+        .. "::"
+        .. "a namespace",
+      path = test_path,
+      name = "a namespace",
+      range = {
+        6,
+        0,
+        32,
+        0,
+      },
+      type = "namespace",
+    }, tree[2][1])
+
+    assert.are.same({
+      id = test_path
+        .. "::"
+        .. "org.example.KotestDescribeSpec"
+        .. "::"
+        .. "a namespace"
+        .. "::"
+        .. "should handle failed assertions",
+      path = test_path,
+      name = "should handle failed assertions",
+      range = {
+        7,
+        0,
+        9,
+        0,
+      },
+      type = "test",
+    }, tree[2][2][1])
+
+    assert.are.same({
+      id = test_path
+        .. "::"
+        .. "org.example.KotestDescribeSpec"
+        .. "::"
+        .. "a namespace"
+        .. "::"
+        .. "should handle passed assertions",
+      path = test_path,
+      name = "should handle passed assertions",
+      range = {
+        11,
+        0,
+        13,
+        0,
+      },
+      type = "test",
+    }, tree[2][3][1])
+  end)
+end)
