792253679921d79a955d49e0e84159f1ab52622c
Nick Hackman <snickhackman@gmail.com>
2026-01-24 17:40:31 -0500

    fix: simplify mockKotestApi
    We only need the classes to be defined, so we can remove all the
    Subclasses and remove the class bodies since there are no definitions.

--- kotlin-test/core/src/main/kotlin/io/github/codymikol/kotlintest/provider/KotestStubVirtualFileProvider.kt
+++ kotlin-test/core/src/main/kotlin/io/github/codymikol/kotlintest/provider/KotestStubVirtualFileProvider.kt
@@ -6,35 +6,16 @@ import org.jetbrains.kotlin.idea.KotlinLanguage
 private val mockKotestApi = """
        package io.kotest.core.spec.style
 
-        class AnnotationSpec {}
-        class AnnotationSpecSubclass : AnnotationSpec
-       
-        class BehaviorSpec {}
-        class BehaviorSpecSubclass : BehaviorSpec
-       
-        class DescribeSpec {}
-        class DescribeSpecSubclass : DescribeSpec
-       
-        class ExpectSpec {}
-        class ExpectSpecSubclass : ExpectSpec
-       
-        class FeatureSpec {}
-        class FeatureSpecSubclass : FeatureSpec
-       
-        class FreeSpec {}
-        class FreeSpecSubclass : FreeSpec
-       
-        class FunSpec {}
-        class FunSpecSubclass : FunSpec
-       
-        class ShouldSpec {}
-        class ShouldSpecSubclass : ShouldSpec
-       
-        class StringSpec {}
-        class StringSpecSubclass : StringSpec
-       
-        class WordSpec {}
-        class WordSpecSubclass : WordSpec
+        class AnnotationSpec
+        class BehaviorSpec
+        class DescribeSpec
+        class ExpectSpec
+        class FeatureSpec
+        class FreeSpec
+        class FunSpec
+        class ShouldSpec
+        class StringSpec
+        class WordSpec
 """.trimIndent()
 
 internal fun kotestStubImplVirtualFile(): LightVirtualFile = LightVirtualFile(
