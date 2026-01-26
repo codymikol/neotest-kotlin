7609598610c73cb6329dd3c15c32cc013cd5f07f
Nick Hackman <snickhackman@gmail.com>
2026-01-25 21:29:56 -0500

    style: remove unused extension method

--- kotlin-test/core/src/main/kotlin/io/github/codymikol/kotlintest/discover/TestDiscoverer.kt
+++ kotlin-test/core/src/main/kotlin/io/github/codymikol/kotlintest/discover/TestDiscoverer.kt
@@ -19,14 +19,6 @@ public interface TestDiscoverer {
                 JUnitTestDiscoverer,
             )
 
-        private fun Collection<DiscoveredResult>.rollup(): DiscoveredResult =
-            this.fold(DiscoveredResult()) { acc, result ->
-                acc.copy(
-                    tests = acc.tests + result.tests,
-                    warnings = acc.warnings + result.warnings
-                )
-            }
-
         /**
          * Main entry point for test discovery. Executes all [discoverers] on the provided [KtFile]s.
          */
