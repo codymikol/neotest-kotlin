5356f476575d7039d32c90b459f30c95c170523f
Nick Hackman <snickhackman@gmail.com>
2026-01-31 18:25:14 -0500

    test: e2e test

---
+++ tests/end_to_end_spec.lua
@@ -0,0 +1,107 @@
+local lib = require("neotest.lib")
+local neotest_kotlin = require("neotest-kotlin")
+local nio = require("nio")
+
+local example_project_path = vim.fs.joinpath(
+  debug.getinfo(1).source:match("@?(.*/)"),
+  "example_project",
+  "app",
+  "src",
+  "test",
+  "kotlin",
+  "org",
+  "example"
+)
+
+describe("neotest-kotlin", function()
+  nio.tests.it("Basic", function()
+    local test_path = vim.fs.joinpath(example_project_path, "KotestFunSpec.kt")
+
+    local tree = neotest_kotlin.discover_positions(test_path)
+    assert.not_nil(tree)
+
+    local spec = neotest_kotlin.build_spec({ tree = tree })
+    assert.not_nil(spec)
+    assert.not_nil(spec.cwd)
+    assert.not_nil(spec.command)
+
+    ---@type string[]
+    local args = {}
+    for arg in spec.command:gmatch("%S+") do
+      table.insert(args, arg)
+    end
+
+    local run_args = {
+      cwd = spec.cwd,
+      cmd = args[1],
+      args = vim.list_slice(args, 2, #args),
+    }
+
+    vim.print("Fork Args:", run_args)
+
+    local process = nio.process.run(run_args)
+
+    assert.not_nil(process)
+
+    local output = process.stdout.read()
+    vim.print("Gradle Output:", output)
+
+    -- assert that our task ran
+    assert.matches(".*> Task :app:kotlinTestExecute.*", output)
+
+    ---@type integer
+    local status_code = process.result(true)
+    assert.equals(0, status_code)
+
+    local results_path = spec.context.results_path
+    vim.print("Test Results Path: " .. results_path)
+    assert.not_nil(lib.files.exists(results_path))
+
+    local results = neotest_kotlin.results(spec, nil, nil)
+
+    assert.not_nil(
+      results[test_path .. "::org.example.KotestFunSpec::namespace::fail"].output
+    )
+
+    -- set to nil for full assertion below
+    results[test_path .. "::org.example.KotestFunSpec::namespace::fail"].output =
+      nil
+
+    assert.not_nil(
+      results[test_path .. "::org.example.KotestFunSpec::namespace::nested namespace::fail"].output
+    )
+
+    -- set to nil for full assertion below
+    results[test_path .. "::org.example.KotestFunSpec::namespace::nested namespace::fail"].output =
+      nil
+
+    assert.are.same({
+      [test_path .. "::org.example.KotestFunSpec::namespace::fail"] = {
+        status = "failed",
+        short = 'expected:<"b"> but was:<"a">',
+        errors = {
+          {
+            message = 'expected:<"b"> but was:<"a">',
+            line = 12,
+          },
+        },
+      },
+      [test_path .. "::org.example.KotestFunSpec::namespace::pass"] = {
+        status = "passed",
+      },
+      [test_path .. "::org.example.KotestFunSpec::namespace::nested namespace::pass"] = {
+        status = "passed",
+      },
+      [test_path .. "::org.example.KotestFunSpec::namespace::nested namespace::fail"] = {
+        status = "failed",
+        short = 'expected:<"b"> but was:<"a">',
+        errors = {
+          {
+            message = 'expected:<"b"> but was:<"a">',
+            line = 21,
+          },
+        },
+      },
+    }, results)
+  end)
+end)
